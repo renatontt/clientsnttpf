37bf02b429a5637e689a49f2e97502485cf99311
Renato <renato.garay@pucp.pe>
2022-05-03 17:24:20 -0500

    Add actuator dependency

--- src/main/java/com/group7/clientsservice/ClientsServiceApplication.java
+++ src/main/java/com/group7/clientsservice/ClientsServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClientsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientsServiceApplication.class, args);
+    }
 
 }
